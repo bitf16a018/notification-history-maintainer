25a57fa445bc4f64ade7145bc1c5903c312eb3ad
Rukhsar Ayub <bitf16a031@pucit.edu.pk>
2019-12-10 01:03:41 +0500

    RECYCLER VIEW VARIABLES SET

--- app/src/main/java/co/dear/notificationhistorymaintainer/MainActivity.java
+++ app/src/main/java/co/dear/notificationhistorymaintainer/MainActivity.java
@@ -3,14 +3,27 @@ package co.dear.notificationhistorymaintainer;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private static final String ACTION_NOTIFICATION_LISTENER_SETTINGS = "android.settings.ACTION_NOTIFICATION_LISTENER_SETTINGS";
+    RecyclerView recyclerView;
+    MyAdapter adapter;
+    List<NotificationModel> notifications = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new MyAdapter(this, notifications);
+        recyclerView.setAdapter(adapter);
 
         AlertDialogUtils.buildNotificationServiceAlertDialog(
                 this,
